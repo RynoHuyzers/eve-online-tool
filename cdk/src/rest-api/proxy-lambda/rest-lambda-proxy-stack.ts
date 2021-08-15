4f76bc5c6d112d360c504e6eff264cd489777dcc
RynoHuyzers <RynoHuyz@gmail.com>
2021-08-15 13:05:04 +0200

    updating proxy lambda

--- cdk/src/rest-api/proxy-lambda/rest-lambda-proxy-stack.ts
+++ cdk/src/rest-api/proxy-lambda/rest-lambda-proxy-stack.ts
@@ -10,15 +10,6 @@ export class ProxyLambda extends Stack{
         const appName = this.node.tryGetContext('AppName');
         const environment = this.node.tryGetContext('Env');
         const accountNumber = this.node.tryGetContext('AWSAccountNumber')
-        // NestJS Rest API Layer
-        const layerArn: IStringParameter = StringParameter.fromStringParameterName(
-            this,
-            'NestJSRestLayerARN',
-            '/ClientManagement/RestApi/NestJsLayer/Arn',
-        );
-        const nestjsLayer: ILayerVersion = LayerVersion.fromLayerVersionAttributes(this, 'NestJSRestLayer', {
-            layerVersionArn: layerArn.stringValue,
-        });
 
         /******** Roles and policies */
         const customLambdaRole = new Role(this, 'analyticsRole', {
@@ -29,29 +20,15 @@ export class ProxyLambda extends Stack{
                 ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'),
                 ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMFullAccess'),
                 ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
-                ManagedPolicy.fromAwsManagedPolicyName('AmazonEventBridgeFullAccess'),
             ],
         });
-        customLambdaRole.addToPolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            resources: [`arn:aws:quicksight:eu-west-1:${accountNumber}:dashboard/*`],
-            actions: ['quicksight:GetDashboardEmbedUrl'],
-        }));
-        customLambdaRole.addToPolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            resources: ['*'],
-            actions: ['quicksight:DescribeUser',
-                'quicksight:RegisterUser',
-                'quicksight:GetAuthCode']
-        }));
 
         // Rest Proxy Function
-        const restProxyFnPath: string = `${__dirname}/../../../../deploy/rest-api/analytics-rest-lambda.zip`;
-        const restProxyLambda: Function = new Function(this, 'Analytics_RestAPI_ProxyLambda', {
-            functionName: 'Analytics-RestProxy',
+        const restProxyFnPath: string = `${__dirname}/../../../../deploy/rest-api/project-template.zip`;
+        const restProxyLambda: Function = new Function(this, 'RestAPI_ProxyLambda', {
+            functionName: 'RestProxy',
             code: new AssetCode(restProxyFnPath),
             handler: 'rest-api/rest-api/src/lambda-rest-handler.handler',
-            layers: [nestjsLayer],
             runtime: Runtime.NODEJS_12_X,
             memorySize: 1024,
             timeout: Duration.seconds(60),
